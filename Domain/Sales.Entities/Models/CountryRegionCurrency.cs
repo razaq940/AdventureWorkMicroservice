f8bfe1ff29a04524bb6b3100f106587c1dae5cc9
nottitan <chimmyforlyfe@gmail.com>
2022-05-11 23:11:28 +0700

    ghghhj

---
+++ Domain/Sales.Entities/Models/CountryRegionCurrency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Sales.Entities.Models
+{
+    public partial class CountryRegionCurrency
+    {
+        public string CountryRegionCode { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual Currency CurrencyCodeNavigation { get; set; }
+    }
+}
---
+++ Domain/Sales.Entities/Models/PersonCreditCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Sales.Entities.Models
+{
+    public partial class PersonCreditCard
+    {
+        public int BusinessEntityId { get; set; }
+        public int CreditCardId { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual CreditCard CreditCard { get; set; }
+    }
+}
